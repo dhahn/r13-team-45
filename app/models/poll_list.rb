08fbcedfb496ab5a27334df1f4735c48fb1c97c2
Brad Rice <bradley_rice@mac.com>
2013-10-19 11:12:49 -0500

    Remove STI from List class

---
+++ app/models/poll_list.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: poll_lists
+#
+#  id       :integer          not null, primary key
+#  user_id  :integer
+#  question :string(255)
+#
+
+class PollList < ActiveRecord::Base
+  attr_accessible :question, :user_id
+
+  has_many :items
+  belongs_to :user
+
+  validates_presence_of :question
+  validates_presence_of :user_id
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131019125718) do
+ActiveRecord::Schema.define(:version => 20131019160339) do
+
+  create_table "check_lists", :force => true do |t|
+    t.integer "user_id"
+    t.string  "title"
+  end
 
   create_table "items", :force => true do |t|
     t.string   "type"
@@ -22,12 +27,9 @@ ActiveRecord::Schema.define(:version => 20131019125718) do
     t.datetime "updated_at",                :null => false
   end
 
-  create_table "lists", :force => true do |t|
-    t.string   "type"
-    t.string   "title"
-    t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "poll_lists", :force => true do |t|
+    t.integer "user_id"
+    t.string  "question"
   end
 
   create_table "notes", :force => true do |t|

8c5f92b08efff2627f176333bd2653cf8d02daa0
David Hahn <dhahn@ctatechs.com>
2013-10-19 14:59:37 -0500

    Housekeeping. Tip plese.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Roommates::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :rooms
-  resources :lists
   post "/invitation" => "invitations#create"
   resources :notes
   resources :pictures

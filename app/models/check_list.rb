c3fecd1bb77848281345ff648cd9d0ad8baf8a27
Brad Rice <bradley_rice@mac.com>
2013-10-19 13:56:50 -0500

    Setup relationships in List models and allow nested attributes

--- app/models/check_list.rb
+++ app/models/check_list.rb
@@ -8,10 +8,12 @@
 #
 
 class CheckList < ActiveRecord::Base
-  attr_accessible :title, :user_id
+  attr_accessible :title, :user_id, :check_list_items_attributes
 
-  has_many :items
+  has_many :check_list_items
   belongs_to :user
+
+  accepts_nested_attributes_for :check_list_items
 
   validates_presence_of :title
   validates_presence_of :user_id

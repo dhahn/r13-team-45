b7534315be64db4733403cb505e97ff1ab2755ba
Brad Rice <bradley_rice@mac.com>
2013-10-20 15:01:21 -0500

    Make bill amounts clickable to toggle paid

--- app/assets/javascripts/bills.js
+++ app/assets/javascripts/bills.js
@@ -21,3 +21,17 @@ $(document).on("change", "#bill_interval", function(){
     $('#monthly_specific_day_of').addClass("hidden");
   }
 });
+
+$(document).on("click", '.bill .amount', function(){
+	var bill_id = $(this).data("bill-id");
+	$.ajax({
+		url: '/bills/' + bill_id + '.json',
+		data: { bill: {value: ($(this).data("value") + 1)} },
+		type: 'PUT',
+		success: function(response) {
+			var bill = $("*[data-bill-id=" + response["id"] + "]");
+			$(bill).data("value", response["value"]);
+			$(bill).toggleClass("line-through");
+		}
+	});
+});
